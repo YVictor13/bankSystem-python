6d21d0075d582b3beb7111b365868166815bb382
YVictor13 <2357772033@qq.com>
2020-05-28 18:56:09 +0800

    add mine

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config["SECRET_KEY"] = "12345678"
 db = SQLAlchemy(app)
 
 
-# 用户类
+# 银行卡类
 class Card(db.Model):
     """银行卡表"""
     __tablename__ = "Card"
@@ -20,8 +20,11 @@ class Card(db.Model):
     password = db.Column(db.String(32), nullable=False)
     money = db.Column(db.String(20), default=0)
     accountId = db.Column(db.String(64), db.ForeignKey("User.accountId"))  # 外键字段
+    Transfer = db.relationship("Transfer", backref="Card", primaryjoin='Card.CardId == Transfer.my_CardId')
+    Deposit = db.relationship("Deposit", backref="Card")
 
 
+# 用户类
 class User(db.Model):
     """用户表"""
     __tablename__ = "User"  # 指明数据库表名
@@ -30,6 +33,25 @@ class User(db.Model):
     email = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     Card = db.relationship("Card", backref="User")
+
+
+class Transfer(db.Model):
+    """转账表"""
+    __tablename__ = "Transfer"  # 指明数据库表名
+    id = db.Column(db.Integer, primary_key=True)  # 主键 整型的主键默认设置为自增
+    my_CardId = db.Column(db.Integer, db.ForeignKey("Card.CardId"))  # 外键字段
+    other_CardId = db.Column(db.Integer, db.ForeignKey("Card.CardId"))  # 外键字段
+    datetime = db.Column(db.DateTime, nullable=False)
+    money = db.Column(db.String(64), nullable=False)
+
+
+class Deposit(db.Model):
+    """存款表"""
+    __tablename__ = "Deposit"
+    id = db.Column(db.Integer, primary_key=True)  # 主键 整型的主键默认设置为自增
+    CardId = db.Column(db.Integer, db.ForeignKey("Card.CardId"))  # 外键字段
+    datetime = db.Column(db.DateTime, nullable=False)
+    money = db.Column(db.String(64), nullable=False)
 
 
 class UserFrom(FlaskForm):
@@ -43,7 +65,7 @@ class UserFrom(FlaskForm):
     submit = SubmitField('提交')
 
 
-# 创建数据表
+# # 创建数据表
 # # 使用drop_all清除数据库中的所有数据
 # db.drop_all()
 # # 创建所有的表
@@ -64,20 +86,31 @@ def login():
 def do_admin_login():
     password = request.form['password']
     accountId = request.form['accountId']
-    obj = User.query.filter_by(accountId=accountId).first()
-    if obj and password == obj.password:
+    User_obj = User.query.filter_by(accountId=accountId).first()
+    Card_obj = Card.query.filter_by(accountId=accountId).first()
+    if Card_obj:
+        session['information_ok'] = True
+
+    if User_obj and password == User_obj.password:
+        session['accountId'] = accountId
         session['logged_in'] = True
-        return index()
+        if session.get('information_ok'):
+            session['login_ok'] = True
+            return redirect('/home')
+        else:
+            return redirect('/index')
     else:
         flash('wrong password!')
         return login()
 
 
+# 注册
 @app.route('/register', methods=['GET'])
 def register():
     return render_template('register.html')
 
 
+# 完成注册
 @app.route('/register', methods=['POST'])
 def do_register():
     password = request.form['password']
@@ -92,22 +125,19 @@ def do_register():
         db.session.add(user_obj)
         db.session.commit()
         flash('注册成功')
-        return login()
+        return redirect('/login')
     except:
         flash('注册失败')
         return register()
 
 
+# 完善信息
 @app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/home', methods=['GET'])
-def home():
-    return render_template('home.html')
-
-
+# 实现完善信息功能
 @app.route('/index', methods=['POST'])
 def update_index():
     CardId = request.form['CardId']
@@ -129,16 +159,38 @@ def update_index():
         try:
             db.session.add(Card_obj)
             db.session.commit()
-            return home()
+            session['login_ok'] = True
+            return redirect('/home')
         except:
             flash('用户账号有误，请核实再输入')
             return index()
 
 
+# 退出功能
 @app.route('/login', methods=['GET'])
 def quite():
     session['logged_in'] = False
+    session['login_ok'] = False
+    session['information_ok'] = False
+    session['accountId'] = ''
     return login()
+
+
+# 银行首页
+@app.route('/home', methods=['GET'])
+def home():
+    if session.get('login_ok'):
+        return render_template('home.html')
+    else:
+        return render_template('404.html')
+
+
+@app.route('/home/mine')
+def mine():
+    accountId = session.get('accountId')
+    User_obj = User.query.filter_by(accountId=accountId).first()
+    Card_obj_list = Card.query.filter_by(accountId=accountId)
+    return render_template('mine.html', User_obj=User_obj, Card_obj_list=Card_obj_list)
 
 
 if __name__ == '__main__':
